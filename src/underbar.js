8bc7b9485e64e2b0fd884f1e3f13ba99fdb03658
Hack Reactor Student <student@hackreactor.com>
2018-01-16 11:37:48 -0800

    Finished test for pluck

--- src/underbar.js
+++ src/underbar.js
@@ -217,6 +217,10 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    
+
+
+
   };
 
   // Determine if the array or object contains a given value (using `===`).
